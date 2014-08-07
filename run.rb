cba7ed86c3d94e65e663dcdc1f9962b2dda77848
Seigo Uchida <spesnova@gmail.com>
2014-08-07 15:12:13 +0900

    Create run.rb

--- run.rb
+++ run.rb
@@ -1,3 +1,54 @@
 require "slack-notifier"
+require "dotenv"
 
+# Using dotenv for debug in local
+Dotenv.load
 
+team     = ENV["WERCKER_SLACK_NOTIFY_TEAM"]
+token    = ENV["WERCKER_SLACK_NOTIFY_TOKEN"]
+channel  = ENV["WERCKER_SLACK_NOTIFY_CHANNEL"]
+username = ENV["WERCKER_SLACK_NOTIFY_USERNAME"]
+
+abort "Please specify the your slack team"    unless team
+abort "Please specify the your slack token"   unless token
+abort "Please specify the your slack channel" unless channel
+username = "Wercker"                          unless username
+
+# See for more details about environment variables that we can use in our steps
+# http://devcenter.wercker.com/articles/steps/variables.html
+app_name   = ENV["WERCKER_APPLICATION_NAME"]
+app_url    = ENV["WERCKER_APPLICATION_URL"]
+build_url  = ENV["WERCKER_BUILD_URL"]
+git_commit = ENV["WERCKER_GIT_COMMIT"]
+git_branch = ENV["WERCKER_GIT_BRANCH"]
+started_by = ENV["WERCKER_STARTED_BY"]
+
+def message(app_name, app_url, build_url, git_commit, git_branch, started_by, status)
+  "[[#{app_name}](#{app_url})] [build(#{git_commit[0,8]})](#{build_url}) of #{git_branch} by #{started_by} #{status}"
+end
+
+def icon_url(status)
+  "https://raw.githubusercontent.com/wantedly/step-pretty-slack-notify/master/icons/#{status}.jpg"
+end
+
+def username_with_status(username, status)
+  "#{username} #{status.capitalize}"
+end
+
+notifier = Slack::Notifier.new(
+  team,
+  token,
+  channel: channel,
+)
+
+res = notifier.ping(
+  message(app_name, app_url, build_url, git_commit, git_branch, started_by, ENV["WERCKER_RESULT"]),
+  icon_url: icon_url(ENV["WERCKER_RESULT"]),
+  username: username_with_status(username, ENV["WERCKER_RESULT"])
+)
+
+case res.code
+when "404" then abort "Subdomain or token not found."
+when "500" then abort res.read_body
+else puts "Notified to Slack #{channel}"
+end
