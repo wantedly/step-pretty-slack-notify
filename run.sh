3b07550980c53554eec033a834eb15a039120d47
Seigo Uchida <spesnova@gmail.com>
2014-09-01 19:08:52 +0900

    Do not install gem as root if rvm/rbenv ruby is enable

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-ruby -v
-gem install slack-notifier -v 0.5.0 --no-ri --no-rdoc
+# If step using system ruby, it should install gem as root.
+# If step using rvm/rbenv ruby, it shouldn't install gem as root.
+# See https://github.com/wantedly/step-pretty-slack-notify/issues/1
+if [ `which ruby` = "/usr/bin/ruby" ]; then
+  ruby -v
+  sudo gem install slack-notifier -v 0.5.0 --no-ri --no-rdoc
+elif which ruby ; then
+  ruby -v
+       gem install slack-notifier -v 0.5.0 --no-ri --no-rdoc
+else
+  echo "You need to use a box that installed ruby."
+fi
 $WERCKER_STEP_ROOT/run.rb
+
+exit 0
