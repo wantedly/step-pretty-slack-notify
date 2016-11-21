8c27018b36cbf1ea9ca3c815462f2b6a70aff024
Seigo Uchida <spesnova@gmail.com>
2016-11-22 04:34:36 +0900

    Refactor run.rb

--- run.rb
+++ run.rb
@@ -2,81 +2,146 @@
 
 require "slack-notifier"
 
+class Build
+  def initialize()
+    # See below docs for more details about wercker's environment variables:
+    # http://devcenter.wercker.com/docs/environment-variables/available-env-vars
+    @app_url           = ENV["WERCKER_APPLICATION_URL"]   || ""
+    @build_url         = ENV["WERCKER_BUILD_URL"]         || ""
+    @run_url           = ENV["WERCKER_RUN_URL"]           || ""
+    @git_owner         = ENV["WERCKER_GIT_OWNER"]         || ""
+    @git_repo          = ENV["WERCKER_GIT_REPOSITORY"]    || ""
+    @git_commit        = ENV["WERCKER_GIT_COMMIT"]        || ""
+    @git_branch        = ENV["WERCKER_GIT_BRANCH"]        || ""
+    @git_domain        = ENV["WERCKER_GIT_DOMAIN"]        || ""
+    @started_by        = ENV["WERCKER_STARTED_BY"]        || ""
+    @result            = ENV["WERCKER_RESULT"]            || ""
+    @deploy_url        = ENV["WERCKER_DEPLOY_URL"]        || ""
+    @deploytarget_name = ENV["WERCKER_DEPLOYTARGET_NAME"] || ""
+
+    # Wercker's new stack uses WERCKER_RUN_URL instead of WERCKER_BUILD_URL
+    if @build_url.empty? && !@run_url.empty?
+      @build_url = @run_url
+    end
+  end
+
+  attr_reader :app_url,
+    :build_url,
+    :run_url,
+    :git_owner,
+    :git_repo,
+    :git_commit,
+    :git_branch,
+    :git_domain,
+    :started_by,
+    :result,
+    :deploy_url,
+    :deploytarget_name
+
+  def git_commit_url
+    case @git_domain
+    when "github.com" then
+      "https://#{@git_domain}/#{@git_owner}/#{@git_repo}/commit/#{@git_commit}"
+    when "bitbucket.org" then
+      "https://#{@git_domain}/#{@git_owner}/#{@git_repo}/commit/#{@git_commit}"
+    else
+      nil
+    end
+  end
+
+  def is_deploy?
+    ENV["DEPLOY"] == "true"
+  end
+
+  def has_passed?
+    @result == "passed"
+  end
+
+  def has_failed?
+    @result == "failed"
+  end
+end
+
+class Message
+  def initialize(build)
+    @build = build
+  end
+
+  def text
+    message = "[build](#{@build.build_url})"
+    message = "[deploy](#{@build.deploy_url}) to #{@build.deploytarget_name}" if @build.is_deploy?
+    message += " for [#{@build.git_owner}/#{@build.git_repo}](#{@build.app_url})"
+    message += " by #{@build.started_by} has #{@build.result}"
+
+    if @build.git_commit_url
+      message += " on #{@build.git_branch}([#{@build.git_commit[0,8]}](#{@build.git_commit_url}))"
+    else
+      message += " on #{@build.git_branch}(#{@build.git_commit[0,8]})"
+    end
+  end
+
+  def fallback
+    message = "build"
+    message = "deploy to #{@build.deploytarget_name}" if @build.is_deploy?
+    message += " for #{@build.git_owner}/#{@build.git_repo}"
+    message += " by #{@build.started_by} has #{@build.result}"
+    message += " on #{@build.git_branch}(#{@build.git_commit[0,8]})"
+  end
+
+  def color
+    if @build.has_passed?
+      'good'
+    else
+      'danger'
+    end
+  end
+end
+
+#
+# Main
+#
 # These environment variables are required/optional for pretty-slack-notify step.
 webhook_url = ENV["WERCKER_PRETTY_SLACK_NOTIFY_WEBHOOK_URL"] || ""
 channel     = ENV["WERCKER_PRETTY_SLACK_NOTIFY_CHANNEL"]     || ""
-username    = ENV["WERCKER_PRETTY_SLACK_NOTIFY_USERNAME"]    || ""
+username    = ENV["WERCKER_PRETTY_SLACK_NOTIFY_USERNAME"]    || "Wercker"
 branches    = ENV["WERCKER_PRETTY_SLACK_NOTIFY_BRANCHES"]    || ""
 notify_on   = ENV["WERCKER_PRETTY_SLACK_NOTIFY_NOTIFY_ON"]   || ""
+icon_url    = "https://secure.gravatar.com/avatar/a08fc43441db4c2df2cef96e0cc8c045?s=140"
+
 abort "Please specify the your slack webhook url" if webhook_url.empty?
-username = "Wercker"                              if username.empty?
 
-# See for more details about environment variables that we can use in our steps.
-# http://devcenter.wercker.com/docs/environment-variables/available-env-vars
-git_owner         = ENV["WERCKER_GIT_OWNER"]
-git_repo          = ENV["WERCKER_GIT_REPOSITORY"]
-app_name          = "#{git_owner}/#{git_repo}"
-app_url           = ENV["WERCKER_APPLICATION_URL"]
-build_url         = ENV["WERCKER_BUILD_URL"] || ""
-run_url           = ENV["WERCKER_RUN_URL"]   || ""
-git_commit        = ENV["WERCKER_GIT_COMMIT"]
-git_branch        = ENV["WERCKER_GIT_BRANCH"]
-started_by        = ENV["WERCKER_STARTED_BY"]
-result            = ENV["WERCKER_RESULT"]
-deploy_url        = ENV["WERCKER_DEPLOY_URL"]
-deploytarget_name = ENV["WERCKER_DEPLOYTARGET_NAME"]
+build = Build.new
 
-if !branches.empty? && Regexp.new(branches) !~ git_branch
-  puts "'#{git_branch}' branch did not match notify branches /#{branches}/"
+if !branches.empty? && Regexp.new(branches) !~ build.git_branch
+  puts "'#{build.git_branch}' branch did not match notify branches /#{branches}/"
   puts "Skipped to notify"
   exit
 end
 
-unless notify_on.empty? || notify_on == result
-  puts "Result '#{result}' did not match notify condition '#{notify_on}'"
+unless notify_on.empty? || notify_on == build.result
+  puts "Result '#{build.result}' did not match notify condition '#{notify_on}'"
   puts "Skipped to notify"
   exit
 end
 
-# Wercker's new stack uses WERCKER_RUN_URL instead of WERCKER_BUILD_URL
-if build_url.empty? && !run_url.empty?
-  build_url = run_url
-end
+notifier = Slack::Notifier.new(webhook_url)
+notifier.username = username
+notifier.channel  = '#' + channel unless channel.empty?
 
-def deploy?
-  ENV["DEPLOY"] == "true"
-end
+message = Message.new(build)
 
-def build_message(app_name, app_url, build_url, git_commit, git_branch, started_by, result)
-  "[build](#{build_url}) (#{git_commit[0,8]}) for [#{app_name}](#{app_url}) by #{started_by} has #{result} on #{git_branch}"
-end
-
-def deploy_message(app_name, app_url, deploy_url, deploytarget_name, git_commit, git_branch, started_by, result)
-  "[deploy](#{deploy_url}) (#{git_commit[0,8]}) to #{deploytarget_name} for [#{app_name}](#{app_url}) by #{started_by} has #{result} on #{git_branch}"
-end
-
-def icon_url
-  "https://secure.gravatar.com/avatar/a08fc43441db4c2df2cef96e0cc8c045?s=140"
-end
-
-notifier = Slack::Notifier.new(
-  webhook_url,
-  username: username,
-)
-
-message = deploy? ?
-  deploy_message(app_name, app_url, deploy_url, deploytarget_name, git_commit, git_branch, started_by, result) :
-  build_message(app_name, app_url, build_url, git_commit, git_branch, started_by, result)
-
-notifier.channel = '#' + channel unless channel.empty?
-
-res = notifier.ping(
-  message,
+response = notifier.ping(
+  nil,
   icon_url: icon_url,
+  attachments: [{
+    fallback: message.fallback,
+    text: notifier.escape(message.text),
+    color: message.color,
+  }]
 )
 
-case res.code
+case response.code
 when "404" then abort "Webhook url not found."
-when "500" then abort res.read_body
+when "500" then abort response.read_body
 else puts "Notified to Slack #{notifier.channel}"
 end
